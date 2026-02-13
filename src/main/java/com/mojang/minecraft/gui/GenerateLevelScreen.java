5112a14cc7e637a3401c3b8cf8252fb6a5dc83fc
Philippe Desjardins <philippe.desjardins@inmediatech.co>
2026-02-12 19:38:59 -0500

    Refactor GenerateLevelScreen.java: Improved readability and style. Renamed obfuscated variables, added Javadocs, updated indentation to tabs, and removed redundant modifiers and casts.

--- src/main/java/com/mojang/minecraft/gui/GenerateLevelScreen.java
+++ src/main/java/com/mojang/minecraft/gui/GenerateLevelScreen.java
@@ -1,38 +1,49 @@
 package com.mojang.minecraft.gui;
 
-import com.mojang.minecraft.gui.Button;
-import com.mojang.minecraft.gui.GuiScreen;
-
+/**
+ * A screen for selecting the size of a new level to generate.
+ */
 public final class GenerateLevelScreen extends GuiScreen {
 
-   private GuiScreen parent;
+	/** The parent screen to return to. */
+	private final GuiScreen parent;
 
+	/**
+	 * Creates a new generate level screen.
+	 *
+	 * @param parent The parent screen.
+	 */
+	public GenerateLevelScreen(GuiScreen parent) {
+		this.parent = parent;
+	}
 
-   public GenerateLevelScreen(GuiScreen var1) {
-      this.parent = var1;
-   }
+	@Override
+	public void onOpen() {
+		this.buttons.clear();
+		this.buttons.add(new Button(0, this.width / 2 - 100, this.height / 4, "Small"));
+		this.buttons.add(new Button(1, this.width / 2 - 100, this.height / 4 + 24, "Normal"));
+		this.buttons.add(new Button(2, this.width / 2 - 100, this.height / 4 + 48, "Huge"));
+		this.buttons.add(new Button(3, this.width / 2 - 100, this.height / 4 + 120, "Cancel"));
+	}
 
-   public final void onOpen() {
-      this.buttons.clear();
-      this.buttons.add(new Button(0, this.width / 2 - 100, this.height / 4, "Small"));
-      this.buttons.add(new Button(1, this.width / 2 - 100, this.height / 4 + 24, "Normal"));
-      this.buttons.add(new Button(2, this.width / 2 - 100, this.height / 4 + 48, "Huge"));
-      this.buttons.add(new Button(3, this.width / 2 - 100, this.height / 4 + 120, "Cancel"));
-   }
+	@Override
+	protected void onButtonClick(Button button) {
+		if (button.id == 3) {
+			this.minecraft.setCurrentScreen(this.parent);
+		} else {
+			this.minecraft.generateLevel(button.id);
+			this.minecraft.setCurrentScreen(null);
+			this.minecraft.grabMouse();
+		}
+	}
 
-   protected final void onButtonClick(Button var1) {
-      if(var1.id == 3) {
-         this.minecraft.setCurrentScreen(this.parent);
-      } else {
-         this.minecraft.generateLevel(var1.id);
-         this.minecraft.setCurrentScreen((GuiScreen)null);
-         this.minecraft.grabMouse();
-      }
-   }
-
-   public final void render(int var1, int var2) {
-      drawFadingBox(0, 0, this.width, this.height, 1610941696, -1607454624);
-      drawCenteredString(this.fontRenderer, "Generate new level", this.width / 2, 40, 16777215);
-      super.render(var1, var2);
-   }
+	@Override
+	public void render(int mouseX, int mouseY) {
+		// Draw semi-transparent background
+		drawFadingBox(0, 0, this.width, this.height, 1610941696, -1607454624);
+		drawCenteredString(this.fontRenderer, "Generate new level", this.width / 2, 40, 16777215);
+		super.render(mouseX, mouseY);
+	}
 }
+
+
