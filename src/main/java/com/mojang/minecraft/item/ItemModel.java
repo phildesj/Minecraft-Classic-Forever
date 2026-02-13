256b47884b10d74b68178f18246857c2f7b69b06
Philippe Desjardins <philippe.desjardins@inmediatech.co>
2026-02-12 20:51:12 -0500

    Refactor ItemModel.java
    Refactor ItemModel.java to improve code quality and documentation.
    - Renamed obfuscated variables like var3, var4, var15 to minCoord/maxCoord.
    - Added Javadocs to the class, constructor, and methods.
    - Added comments explaining vertex and texture coordinate definitions.
    - Renamed texture coordinate variables to uMin, vMin, uMax, vMax to avoid conflicts with vertex names.
    - Fixed indentation to use tabs and ensured Egyptian-style curly brackets.
    - Marked the 'model' field as final.

--- src/main/java/com/mojang/minecraft/item/ItemModel.java
+++ src/main/java/com/mojang/minecraft/item/ItemModel.java
@@ -4,59 +4,74 @@ import com.mojang.minecraft.model.ModelPart;
 import com.mojang.minecraft.model.TexturedQuad;
 import com.mojang.minecraft.model.Vertex;
 
-public class ItemModel
-{
-	public ItemModel(int tex)
-	{
-		float var3 = -2.0F;
-		float var4 = -2.0F;
-		float var15 = -2.0F;
+/**
+ * Represents a 3D model for an item in the world.
+ * This is typically rendered as a small cube showing a portion of the terrain texture.
+ */
+public class ItemModel {
+
+	/** The model part used to render the item. */
+	private final ModelPart model = new ModelPart(0, 0);
+
+	/**
+	 * Creates a new ItemModel.
+	 *
+	 * @param textureIndex The index of the texture in the terrain sprite sheet.
+	 */
+	public ItemModel(int textureIndex) {
+		float minCoord = -2.0F;
+		float maxCoord = 2.0F;
 
 		model.vertices = new Vertex[8];
 		model.quads = new TexturedQuad[6];
 
-		Vertex vertex1 = new Vertex(var15, var4, var3, 0.0F, 0.0F);
-		Vertex vertex2 = new Vertex(2.0F, var4, var3, 0.0F, 8.0F);
-		Vertex vertex3 = new Vertex(2.0F, 2.0F, var3, 8.0F, 8.0F);
-		Vertex vertex4 = new Vertex(var15, 2.0F, var3, 8.0F, 0.0F);
-		Vertex vertex5 = new Vertex(var15, var4, 2.0F, 0.0F, 0.0F);
-		Vertex vertex6 = new Vertex(2.0F, var4, 2.0F, 0.0F, 8.0F);
-		Vertex vertex7 = new Vertex(2.0F, 2.0F, 2.0F, 8.0F, 8.0F);
-		Vertex vertex8 = new Vertex(var15, 2.0F, 2.0F, 8.0F, 0.0F);
+		// Define the 8 vertices of the cube
+		Vertex v1 = new Vertex(minCoord, minCoord, minCoord, 0.0F, 0.0F);
+		Vertex v2 = new Vertex(maxCoord, minCoord, minCoord, 0.0F, 8.0F);
+		Vertex v3 = new Vertex(maxCoord, maxCoord, minCoord, 8.0F, 8.0F);
+		Vertex v4 = new Vertex(minCoord, maxCoord, minCoord, 8.0F, 0.0F);
+		Vertex v5 = new Vertex(minCoord, minCoord, maxCoord, 0.0F, 0.0F);
+		Vertex v6 = new Vertex(maxCoord, minCoord, maxCoord, 0.0F, 8.0F);
+		Vertex v7 = new Vertex(maxCoord, maxCoord, maxCoord, 8.0F, 8.0F);
+		Vertex v8 = new Vertex(minCoord, maxCoord, maxCoord, 8.0F, 0.0F);
 
-		model.vertices[0] = vertex1;
-		model.vertices[1] = vertex2;
-		model.vertices[2] = vertex3;
-		model.vertices[3] = vertex4;
-		model.vertices[4] = vertex5;
-		model.vertices[5] = vertex6;
-		model.vertices[6] = vertex7;
-		model.vertices[7] = vertex8;
+		model.vertices[0] = v1;
+		model.vertices[1] = v2;
+		model.vertices[2] = v3;
+		model.vertices[3] = v4;
+		model.vertices[4] = v5;
+		model.vertices[5] = v6;
+		model.vertices[6] = v7;
+		model.vertices[7] = v8;
 
-		float u1 = ((float)(tex % 16) + (1.0F - 0.25F)) / 16.0F;
-		float v1 = ((float)(tex / 16) + (1.0F - 0.25F)) / 16.0F;
-		float u2 = ((float)(tex % 16) + 0.25F) / 16.0F;
-		float v2 = ((float)(tex / 16) + 0.25F) / 16.0F;
+		// Calculate UV coordinates for the texture index
+		// The 0.25F and 0.75F offsets seem to be shrinking the texture area slightly to avoid bleeding?
+		// Or it's a specific coordinate mapping for the item model size.
+		float uMin = ((float) (textureIndex % 16) + (1.0F - 0.25F)) / 16.0F;
+		float vMin = ((float) (textureIndex / 16) + (1.0F - 0.25F)) / 16.0F;
+		float uMax = ((float) (textureIndex % 16) + 0.25F) / 16.0F;
+		float vMax = ((float) (textureIndex / 16) + 0.25F) / 16.0F;
 
-		Vertex[] vertexes1 = new Vertex[] {vertex6, vertex2, vertex3, vertex7};
-		Vertex[] vertexes2 = new Vertex[] {vertex1, vertex5, vertex8, vertex4};
-		Vertex[] vertexes3 = new Vertex[] {vertex6, vertex5, vertex1, vertex2};
-		Vertex[] vertexes4 = new Vertex[] {vertex3, vertex4, vertex8, vertex7};
-		Vertex[] vertexes5 = new Vertex[] {vertex2, vertex1, vertex4, vertex3};
-		Vertex[] vertexes6 = new Vertex[] {vertex5, vertex6, vertex7, vertex8};
+		// Define the 6 faces of the cube
+		Vertex[] faceRight = new Vertex[]{v6, v2, v3, v7};
+		Vertex[] faceLeft = new Vertex[]{v1, v5, v8, v4};
+		Vertex[] faceBottom = new Vertex[]{v6, v5, v1, v2};
+		Vertex[] faceTop = new Vertex[]{v3, v4, v8, v7};
+		Vertex[] faceBack = new Vertex[]{v2, v1, v4, v3};
+		Vertex[] faceFront = new Vertex[]{v5, v6, v7, v8};
 
-		model.quads[0] = new TexturedQuad(vertexes1, u1, v1, u2, v2);
-		model.quads[1] = new TexturedQuad(vertexes2, u1, v1, u2, v2);
-		model.quads[2] = new TexturedQuad(vertexes3, u1, v1, u2, v2);
-		model.quads[3] = new TexturedQuad(vertexes4, u1, v1, u2, v2);
-		model.quads[4] = new TexturedQuad(vertexes5, u1, v1, u2, v2);
-		model.quads[5] = new TexturedQuad(vertexes6, u1, v1, u2, v2);
+		model.quads[0] = new TexturedQuad(faceRight, uMin, vMin, uMax, vMax);
+		model.quads[1] = new TexturedQuad(faceLeft, uMin, vMin, uMax, vMax);
+		model.quads[2] = new TexturedQuad(faceBottom, uMin, vMin, uMax, vMax);
+		model.quads[3] = new TexturedQuad(faceTop, uMin, vMin, uMax, vMax);
+		model.quads[4] = new TexturedQuad(faceBack, uMin, vMin, uMax, vMax);
+		model.quads[5] = new TexturedQuad(faceFront, uMin, vMin, uMax, vMax);
 	}
 
-	private ModelPart model = new ModelPart(0, 0);
-
-	public void generateList()
-	{
+	/**
+	 * Renders the item model using its display list.
+	 */
+	public void generateList() {
 		model.render(0.0625F);
 	}
 }
