1d4a67c4024ce1f85d96061fb2f5c3aa6efccd74
Philippe Desjardins <philippe.desjardins@inmediatech.co>
2026-02-12 19:18:19 -0500

    Refactor ChatInputScreen.java: Improved readability, style, and code quality. Renamed obfuscated variables, added Javadocs, updated indentation to tabs, replaced magic numbers with Keyboard constants, and fixed several lint warnings (unused imports, unnecessarily escaped characters, and use of isEmpty()).

--- src/main/java/com/mojang/minecraft/gui/ChatInputScreen.java
+++ src/main/java/com/mojang/minecraft/gui/ChatInputScreen.java
@@ -1,69 +1,83 @@
 package com.mojang.minecraft.gui;
 
-import com.mojang.minecraft.gui.GuiScreen;
-import com.mojang.minecraft.net.NetworkManager;
 import com.mojang.minecraft.net.PacketType;
 import org.lwjgl.input.Keyboard;
 
+/**
+ * A screen for typing and sending chat messages.
+ */
 public final class ChatInputScreen extends GuiScreen {
 
-   private String message = "";
-   private int counter = 0;
+	/** The current message being typed. */
+	private String message = "";
+	/** A counter used for the blinking cursor effect. */
+	private int counter = 0;
+	/** Allowed characters in chat. */
+	private static final String ALLOWED_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ,.:-_'*!\\\"#%/()=+?[]{}<>@|$;";
 
+	@Override
+	public void onOpen() {
+		Keyboard.enableRepeatEvents(true);
+	}
 
-   public final void onOpen() {
-      Keyboard.enableRepeatEvents(true);
-   }
+	@Override
+	public void onClose() {
+		Keyboard.enableRepeatEvents(false);
+	}
 
-   public final void onClose() {
-      Keyboard.enableRepeatEvents(false);
-   }
+	@Override
+	public void tick() {
+		++this.counter;
+	}
 
-   public final void tick() {
-      ++this.counter;
-   }
+	@Override
+	protected void onKeyPress(char typedChar, int keyCode) {
+		if (keyCode == Keyboard.KEY_ESCAPE) {
+			this.minecraft.setCurrentScreen(null);
+		} else if (keyCode == Keyboard.KEY_RETURN) {
+			String trimmedMessage = this.message.trim();
 
-   protected final void onKeyPress(char var1, int var2) {
-      if(var2 == 1) {
-         this.minecraft.setCurrentScreen((GuiScreen)null);
-      } else if(var2 == 28) {
-         NetworkManager var10000 = this.minecraft.networkManager;
-         String var4 = this.message.trim();
-         NetworkManager var3 = var10000;
-         if((var4 = var4.trim()).length() > 0) {
-            var3.netHandler.send(PacketType.CHAT_MESSAGE, new Object[]{Integer.valueOf(-1), var4});
-         }
+			if (!trimmedMessage.isEmpty()) {
+				this.minecraft.networkManager.netHandler.send(PacketType.CHAT_MESSAGE, -1, trimmedMessage);
+			}
 
-         this.minecraft.setCurrentScreen((GuiScreen)null);
-      } else {
-         if(var2 == 14 && this.message.length() > 0) {
-            this.message = this.message.substring(0, this.message.length() - 1);
-         }
+			this.minecraft.setCurrentScreen(null);
+		} else {
+			if (keyCode == Keyboard.KEY_BACK && !this.message.isEmpty()) {
+				this.message = this.message.substring(0, this.message.length() - 1);
+			}
 
-         if("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ,.:-_\'*!\\\"#%/()=+?[]{}<>@|$;".indexOf(var1) >= 0 && this.message.length() < 64 - (this.minecraft.session.username.length() + 2)) {
-            this.message = this.message + var1;
-         }
+			if (ALLOWED_CHARS.indexOf(typedChar) >= 0 && this.message.length() < 64 - (this.minecraft.session.username.length() + 2)) {
+				this.message = this.message + typedChar;
+			}
+		}
+	}
 
-      }
-   }
+	@Override
+	public void render(int mouseX, int mouseY) {
+		// Draw the chat input box at the bottom
+		drawBox(2, this.height - 14, this.width - 2, this.height - 2, Integer.MIN_VALUE);
 
-   public final void render(int var1, int var2) {
-      drawBox(2, this.height - 14, this.width - 2, this.height - 2, Integer.MIN_VALUE);
-      drawString(this.fontRenderer, "> " + this.message + (this.counter / 6 % 2 == 0?"_":""), 4, this.height - 12, 14737632);
-   }
+		// Blinking cursor logic
+		String cursor = (this.counter / 6 % 2 == 0) ? "_" : "";
+		drawString(this.fontRenderer, "> " + this.message + cursor, 4, this.height - 12, 14737632);
+	}
 
-   protected final void onMouseClick(int var1, int var2, int var3) {
-      if(var3 == 0 && this.minecraft.hud.hoveredPlayer != null) {
-         if(this.message.length() > 0 && !this.message.endsWith(" ")) {
-            this.message = this.message + " ";
-         }
+	@Override
+	protected void onMouseClick(int mouseX, int mouseY, int button) {
+		if (button == 0 && this.minecraft.hud.hoveredPlayer != null) {
+			if (!this.message.isEmpty() && !this.message.endsWith(" ")) {
+				this.message = this.message + " ";
+			}
 
-         this.message = this.message + this.minecraft.hud.hoveredPlayer;
-         var1 = 64 - (this.minecraft.session.username.length() + 2);
-         if(this.message.length() > var1) {
-            this.message = this.message.substring(0, var1);
-         }
-      }
-
-   }
+			this.message = this.message + this.minecraft.hud.hoveredPlayer;
+			int maxLength = 64 - (this.minecraft.session.username.length() + 2);
+			if (this.message.length() > maxLength) {
+				this.message = this.message.substring(0, maxLength);
+			}
+		}
+	}
 }
+
+
+
