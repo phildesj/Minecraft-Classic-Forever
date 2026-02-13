5d73f183c734e960d84094176b2c4012683ee5cd
Philippe Desjardins <philippe.desjardins@inmediatech.co>
2026-02-12 19:43:27 -0500

    Refactor GuiScreen.java: Improved readability, style, and documentation. Renamed obfuscated variables, added Javadocs, updated indentation to tabs, used generic types for the buttons list, and cleaned up rendering logic.

--- src/main/java/com/mojang/minecraft/gui/GuiScreen.java
+++ src/main/java/com/mojang/minecraft/gui/GuiScreen.java
@@ -1,117 +1,165 @@
 package com.mojang.minecraft.gui;
 
 import com.mojang.minecraft.Minecraft;
-import com.mojang.minecraft.gui.Button;
-import com.mojang.minecraft.gui.FontRenderer;
-import com.mojang.minecraft.gui.Screen;
 import java.util.ArrayList;
 import java.util.List;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.GL11;
 
+/**
+ * The base class for all GUI screens in the game.
+ */
 public class GuiScreen extends Screen {
 
-   protected Minecraft minecraft;
-   protected int width;
-   protected int height;
-   protected List buttons = new ArrayList();
-   public boolean grabsMouse = false;
-   protected FontRenderer fontRenderer;
+	/** Reference to the Minecraft instance. */
+	protected Minecraft minecraft;
+	/** The width of the screen. */
+	protected int width;
+	/** The height of the screen. */
+	protected int height;
+	/** A list of buttons on this screen. */
+	protected List<Button> buttons = new ArrayList<>();
+	/** Whether the screen grabs the mouse cursor. */
+	public boolean grabsMouse = false;
+	/** Reference to the font renderer for drawing text. */
+	protected FontRenderer fontRenderer;
 
+	/**
+	 * Renders the screen and its components.
+	 *
+	 * @param mouseX The X coordinate of the mouse.
+	 * @param mouseY The Y coordinate of the mouse.
+	 */
+	public void render(int mouseX, int mouseY) {
+		for (Button button : this.buttons) {
+			if (button.visible) {
+				GL11.glBindTexture(GL11.GL_TEXTURE_2D, this.minecraft.textureManager.load("/gui/gui.png"));
+				GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
 
-   public void render(int var1, int var2) {
-      for(int var3 = 0; var3 < this.buttons.size(); ++var3) {
-         Button var10000 = (Button)this.buttons.get(var3);
-         Minecraft var7 = this.minecraft;
-         Button var4 = var10000;
-         if(var10000.visible) {
-            FontRenderer var8 = var7.fontRenderer;
-            GL11.glBindTexture(3553, var7.textureManager.load("/gui/gui.png"));
-            GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-            byte var9 = 1;
-            boolean var6 = var1 >= var4.x && var2 >= var4.y && var1 < var4.x + var4.width && var2 < var4.y + var4.height;
-            if(!var4.active) {
-               var9 = 0;
-            } else if(var6) {
-               var9 = 2;
-            }
+				byte state = 1; // Default state
+				boolean hovered = mouseX >= button.x && mouseY >= button.y && mouseX < button.x + button.width && mouseY < button.y + button.height;
 
-            var4.drawImage(var4.x, var4.y, 0, 46 + var9 * 20, var4.width / 2, var4.height);
-            var4.drawImage(var4.x + var4.width / 2, var4.y, 200 - var4.width / 2, 46 + var9 * 20, var4.width / 2, var4.height);
-            if(!var4.active) {
-               Button.drawCenteredString(var8, var4.text, var4.x + var4.width / 2, var4.y + (var4.height - 8) / 2, -6250336);
-            } else if(var6) {
-               Button.drawCenteredString(var8, var4.text, var4.x + var4.width / 2, var4.y + (var4.height - 8) / 2, 16777120);
-            } else {
-               Button.drawCenteredString(var8, var4.text, var4.x + var4.width / 2, var4.y + (var4.height - 8) / 2, 14737632);
-            }
-         }
-      }
+				if (!button.active) {
+					state = 0; // Disabled state
+				} else if (hovered) {
+					state = 2; // Hovered state
+				}
 
-   }
+				button.drawImage(button.x, button.y, 0, 46 + state * 20, button.width / 2, button.height);
+				button.drawImage(button.x + button.width / 2, button.y, 200 - button.width / 2, 46 + state * 20, button.width / 2, button.height);
 
-   protected void onKeyPress(char var1, int var2) {
-      if(var2 == 1) {
-         this.minecraft.setCurrentScreen((GuiScreen)null);
-         this.minecraft.grabMouse();
-      }
+				int color = 14737632; // Normal color
+				if (!button.active) {
+					color = -6250336; // Disabled color
+				} else if (hovered) {
+					color = 16777120; // Hovered color
+				}
 
-   }
+				Button.drawCenteredString(this.fontRenderer, button.text, button.x + button.width / 2, button.y + (button.height - 8) / 2, color);
+			}
+		}
+	}
 
-   protected void onMouseClick(int var1, int var2, int var3) {
-      if(var3 == 0) {
-         for(var3 = 0; var3 < this.buttons.size(); ++var3) {
-            Button var4;
-            Button var7;
-            if((var7 = var4 = (Button)this.buttons.get(var3)).active && var1 >= var7.x && var2 >= var7.y && var1 < var7.x + var7.width && var2 < var7.y + var7.height) {
-               this.onButtonClick(var4);
-            }
-         }
-      }
+	/**
+	 * Handles key press events.
+	 *
+	 * @param typedChar The character typed.
+	 * @param keyCode   The code of the key pressed.
+	 */
+	protected void onKeyPress(char typedChar, int keyCode) {
+		if (keyCode == Keyboard.KEY_ESCAPE) {
+			this.minecraft.setCurrentScreen(null);
+			this.minecraft.grabMouse();
+		}
+	}
 
-   }
+	/**
+	 * Handles mouse click events.
+	 *
+	 * @param mouseX      The X coordinate of the mouse.
+	 * @param mouseY      The Y coordinate of the mouse.
+	 * @param mouseButton The button that was clicked.
+	 */
+	protected void onMouseClick(int mouseX, int mouseY, int mouseButton) {
+		if (mouseButton == 0) { // Left click
+			for (Button button : this.buttons) {
+				if (button.active && mouseX >= button.x && mouseY >= button.y && mouseX < button.x + button.width && mouseY < button.y + button.height) {
+					this.onButtonClick(button);
+				}
+			}
+		}
+	}
 
-   protected void onButtonClick(Button var1) {}
+	/**
+	 * Called when a button on the screen is clicked.
+	 *
+	 * @param button The button that was clicked.
+	 */
+	protected void onButtonClick(Button button) {}
 
-   public final void open(Minecraft var1, int var2, int var3) {
-      this.minecraft = var1;
-      this.fontRenderer = var1.fontRenderer;
-      this.width = var2;
-      this.height = var3;
-      this.onOpen();
-   }
+	/**
+	 * Opens the screen and initializes its dimensions.
+	 *
+	 * @param minecraft The Minecraft instance.
+	 * @param width     The initial width.
+	 * @param height    The initial height.
+	 */
+	public final void open(Minecraft minecraft, int width, int height) {
+		this.minecraft = minecraft;
+		this.fontRenderer = minecraft.fontRenderer;
+		this.width = width;
+		this.height = height;
+		this.onOpen();
+	}
 
-   public void onOpen() {}
+	/**
+	 * Called when the screen is opened.
+	 */
+	public void onOpen() {}
 
-   public final void doInput() {
-      while(Mouse.next()) {
-         this.mouseEvent();
-      }
+	/**
+	 * Processes mouse and keyboard input.
+	 */
+	public final void doInput() {
+		while (Mouse.next()) {
+			this.mouseEvent();
+		}
 
-      while(Keyboard.next()) {
-         this.keyboardEvent();
-      }
+		while (Keyboard.next()) {
+			this.keyboardEvent();
+		}
+	}
 
-   }
+	/**
+	 * Processes a single mouse event.
+	 */
+	public final void mouseEvent() {
+		if (Mouse.getEventButtonState()) {
+			int mouseX = Mouse.getEventX() * this.width / this.minecraft.width;
+			int mouseY = this.height - Mouse.getEventY() * this.height / this.minecraft.height - 1;
+			this.onMouseClick(mouseX, mouseY, Mouse.getEventButton());
+		}
+	}
 
-   public final void mouseEvent() {
-      if(Mouse.getEventButtonState()) {
-         int var1 = Mouse.getEventX() * this.width / this.minecraft.width;
-         int var2 = this.height - Mouse.getEventY() * this.height / this.minecraft.height - 1;
-         this.onMouseClick(var1, var2, Mouse.getEventButton());
-      }
+	/**
+	 * Processes a single keyboard event.
+	 */
+	public final void keyboardEvent() {
+		if (Keyboard.getEventKeyState()) {
+			this.onKeyPress(Keyboard.getEventCharacter(), Keyboard.getEventKey());
+		}
+	}
 
-   }
+	/**
+	 * Called every game tick to update the screen.
+	 */
+	public void tick() {}
 
-   public final void keyboardEvent() {
-      if(Keyboard.getEventKeyState()) {
-         this.onKeyPress(Keyboard.getEventCharacter(), Keyboard.getEventKey());
-      }
-
-   }
-
-   public void tick() {}
-
-   public void onClose() {}
+	/**
+	 * Called when the screen is closed.
+	 */
+	public void onClose() {}
 }
+
+
